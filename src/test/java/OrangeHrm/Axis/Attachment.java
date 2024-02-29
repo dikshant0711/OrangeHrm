e05f8319e3939411032fa1e2db98f537d7f5e058
dikshant0711 <dikshantyerwal0711@gmail.com>
2024-02-29 21:54:47 +0530

    first commit

---
+++ src/test/java/OrangeHrm/Axis/Attachment.java
@@ -0,0 +1,34 @@
+package OrangeHrm.Axis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+
+public class Attachment extends Login{
+
+	@AfterMethod
+	public void AddAttach() {
+		WebDriver driver = Login.driver;
+		WebElement immi=driver.findElement(By.xpath("(//div[@class=\"orangehrm-tabs-wrapper\"])[5]"));
+		immi.click();
+		System.out.println("Clicked on Immigration");
+		WebElement Addattach = driver.findElement(By.xpath("(//button[@class=\"oxd-button oxd-button--medium oxd-button--text\"])[2]"));
+		Addattach.click();
+		System.out.println("Clicked on Add Attachemnts");
+		WebElement Browse = driver.findElement(By.xpath("(//input[@type='file'])[1]"));
+		Browse.sendKeys("C:\\Users\\Abhijit Deshmukh\\OneDrive\\Pictures\\Screenshots\\223455.png");
+		
+		WebElement comments = driver.findElement(By.xpath("//textarea[@placeholder='Type comment here']"));
+		comments.click();
+		comments.sendKeys("File Uploaded");
+		System.out.println("Box");
+		WebElement save = driver.findElement(By.xpath("//button[normalize-space()='Save']"));
+		save.click();
+		System.out.println("Attachment is Saved");
+
+		
+		
+}
+
+}
---
+++ src/test/java/OrangeHrm/Axis/Login.java
@@ -0,0 +1,44 @@
+package OrangeHrm.Axis;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login{
+	public static WebDriver driver;
+@SuppressWarnings("deprecation")
+@Test
+public void Log() throws InterruptedException 
+{
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\Dikshant\\Documents\\Manipal\\chromedriver-win64\\chromedriver.exe");
+    driver = new ChromeDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	System.out.println("Page opened");
+	WebElement Log1 = driver.findElement(By.xpath("//input[@name='username']"));	
+	Log1.click();
+	Log1.sendKeys("Admin");
+	System.out.println("Entered Username");
+	
+	WebElement pass = driver.findElement(By.xpath("//input[@name='password']"));
+	pass.click();
+	pass.sendKeys("admin123");
+	System.out.println("Entered Password");
+	
+	WebElement submit = driver.findElement(By.xpath("//button[normalize-space()='Login']"));
+	submit.click();
+	System.out.println("Submitted");
+	
+	WebElement myInfo = driver.findElement(By.xpath("(//li[@class='oxd-main-menu-item-wrapper'])[6]"));
+	myInfo.click();
+	System.out.println("Navigated to My info");
+	
+}
+
+}
